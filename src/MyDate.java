d4fbb9c63312f35dd29a0aca1b3431d34251e79d
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2022-04-25 18:41:10 -0400

    Update MyDate.java

--- src/MyDate.java
+++ src/MyDate.java
@@ -3,7 +3,7 @@ public class MyDate {
     int month;
     int year;
 
-    { //Initialization block that sets a 'default' date if the no-args constructor is called
+    { //Initialization block that sets a 'default' date when ever an object is initialized, the no-args constructor will all the use of this information
         day = 1;
         month = 1;
         year = 2000;
